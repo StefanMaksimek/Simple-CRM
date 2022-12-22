08194e33de12c2bfb53af73382256451ce4ca216
StefanMaksimek <stefan.maksimek@gmail.com>
2022-12-22 11:42:19 +0100

    responsive mat-drawer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  activeSide: boolean = true;
+  mode: any = 'side';
+
+  ngOnInit() {
+    this.setMatDrawer(window.innerWidth);
+  }
+
+  onResize(event: any) {
+    this.setMatDrawer(event.target.innerWidth);
+  }
+
+  setMatDrawer(windowInnerWidth: number) {
+    if (windowInnerWidth < 1100) {
+      this.mode = 'over';
+      this.activeSide = false;
+    } else {
+      this.mode = 'side';
+      this.activeSide = true;
+    }
+  }
+}
